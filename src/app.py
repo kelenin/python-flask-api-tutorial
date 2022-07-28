fa46d847f6595cf1cbe14fce4a827c85c6a5e57e
Katheren Salom <katherensalom@gmail.com>
2022-07-28 23:19:49 +0000

    new file:   src/app.py

---
+++ src/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, jsonify, request, json
+app = Flask(__name__)
+
+todos = [
+    { "label": "My first task", "done": False },
+    { "label": "My second task", "done": False }
+]
+
+data = [
+    { "done": "true", "label": "Sample Todo 1"}
+]
+
+@app.route('/todos', methods=['GET'])
+def hello_world():
+    return jsonify(todos)
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.data
+    decoded_object = json.loads(request_body)
+    print("Incoming request with the following body", request_body)
+    return jsonify(todos)
+
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
